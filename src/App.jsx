e18462b6a9df660d868ee64d5f3db3a5529ceef4
azmansarker00 <azmansarker861@gmail.com>
2025-03-17 01:18:03 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
+import React from "react";
 
 // CSS
-import "./App.css"
+import "./App.css";
 
 // React Router
 import { useEffect } from "react";
 
 // Components
-import Navbar1 from './components/header/nav1/navbar.jsx'
-import HomeMenu from './components/main_body/home_menu/homeMenu.jsx'
-import Navbar2 from './components/header/nav2/navbar.jsx'
-import Text1 from "./components/main_body/texts/text1/text1.jsx"
-import About from "./components/main_body/hero/aboutme/about.jsx"
-import RollingText from "./components/main_body/texts/rollingText/rollingText.jsx"
-import Contact from "./components/main_body/hero/contact/contact.jsx"
+import Navbar1 from "./components/header/nav1/navbar.jsx";
+import HomeMenu from "./components/main_body/home_menu/homeMenu.jsx";
+import Navbar2 from "./components/header/nav2/navbar.jsx";
+import Text1 from "./components/main_body/texts/text1/text1.jsx";
+import About from "./components/main_body/hero/aboutme/about.jsx";
+import RollingText from "./components/main_body/texts/rollingText/rollingText.jsx";
+import Contact from "./components/main_body/hero/contact/contact.jsx";
+import Footer from "./components/footer/footer.jsx";
+
+// alert page
+import Alert from "./components/alert/pagealert.jsx";
 
 
-import Footer from './components/footer/footer.jsx'
 
 function App() {
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  
+
   return (
     <>
-      <Navbar1/>
+      <Alert />
 
-      <HomeMenu/>
+      <Navbar1 />
 
-      <Navbar2/>
+      <HomeMenu />
+
+      <Navbar2 />
 
       <Text1 />
 
@@ -43,10 +47,10 @@ function App() {
       <Contact />
 
       <RollingText />
-      
+
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/alert/pagealert.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./pagealert.css"
+
+function pagealert() {
+  return (
+    <>
+    <div className="alert">
+      <h1>404</h1>
+      <h3>THIS PORTFOLIO IS ONLY FOR DESKTOP</h3>
+    </div>
+    </>
+  )
+}
+
+export default pagealert
